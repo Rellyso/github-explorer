6a4e7b102b6dcebc4905f16358335281462f6df5
rellyso <rellysondouglas2015@gmail.com>
2021-03-09 09:04:51 -0300

    Creating repository list components

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,13 @@
+export function RepositoryItem(props) {
+  return (
+    <li>
+      <strong>{props.repository?.name ?? 'Default'}</strong>
+      <p>{props.repository?.description}</p>
+
+      <a href={props.repository?.link} target="_blank" >
+        Acessar repositório
+      </a>
+
+    </li>
+  )
+}
