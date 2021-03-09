de79eea60087074ea900d34ba13c0f720d888c75
rellyso <rellysondouglas2015@gmail.com>
2021-03-09 09:55:15 -0300

    getting repositories with useEffect hook and fetch

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,10 @@
 import { RepositoryItem } from './RepositoryItem'
 
+import '../styles/repositories.scss'
+import { useEffect, useState } from 'react'
+
+// https://api.github.com/orgs/rocketseat/repos
+
 const repository = {
   name: 'unform',
   description: 'Forms in React',
@@ -7,6 +12,14 @@ const repository = {
 }
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, [])
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
